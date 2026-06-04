51e443cc31604973c5aa185a97f8b1d925d0ccc0
AfanasevStas <afanasevstas96@mail.ru>
2026-06-04 14:06:12 +0300

    Server - FormatLastError DONE

--- Server/main.cpp
+++ Server/main.cpp
@@ -8,15 +8,20 @@
 #include<WinSock2.h>
 #include<WS2tcpip.h>
 #include<iphlpapi.h>
+
+#include<FormatLastError.h>
 using namespace std;
 
 #pragma comment(lib,"WS2_32.lib")
+#pragma comment(lib, "FormatLastError.lib")
 #define MTU 1500
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	cout << "Im - SERVER" << endl;
+	DWORD dwError = 0;
+	CHAR szError[256] = {};
 	WSADATA wsaData;
 	INT iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (iResult != 0)
@@ -44,18 +49,22 @@ void main()
 	}
 
 	SOCKET listen_socket = socket(target->ai_family, target->ai_socktype, target->ai_protocol);
+	dwError = WSAGetLastError();
 	if (listen_socket == INVALID_SOCKET)
 	{
-		cout << "SOCKET creation failed with error: " << WSAGetLastError() << endl;
+		cout << FormatLastError(dwError, szError) << endl;
+		//cout << "SOCKET creation failed with error: " << WSAGetLastError() << endl;
 		freeaddrinfo(target);
 		WSACleanup();
 		return;
 	}
 
 	iResult = bind(listen_socket, target->ai_addr, target->ai_addrlen);
+	dwError = WSAGetLastError();
 	if (iResult != 0)
 	{
-		cout << "bind failed with error: " << WSAGetLastError() << endl;
+		cout << FormatLastError(dwError, szError) << endl;
+		//cout << "bind failed with error: " << WSAGetLastError() << endl;
 		closesocket(listen_socket);
 		freeaddrinfo(target);
 		WSACleanup();
@@ -64,7 +73,9 @@ void main()
 
 	if (listen(listen_socket, 1) == SOCKET_ERROR)
 	{
-		cout << "Listen failed with error: " << WSAGetLastError() << endl;
+		dwError = WSAGetLastError();
+		cout << FormatLastError(dwError, szError) << endl;
+		//cout << "Listen failed with error: " << WSAGetLastError() << endl;
 		closesocket(listen_socket);
 		freeaddrinfo(target);
 		WSACleanup();
@@ -74,7 +85,9 @@ void main()
 	SOCKET client_socket = accept(listen_socket, NULL, NULL);
 	if (client_socket == INVALID_SOCKET)
 	{
-		cout << "Accept failed with error: " << WSAGetLastError() << endl;
+		dwError = WSAGetLastError();
+		cout << FormatLastError(dwError, szError) << endl;
+		//cout << "Accept failed with error: " << WSAGetLastError() << endl;
 		closesocket(listen_socket);
 		freeaddrinfo(target);
 		WSACleanup();
@@ -94,18 +107,27 @@ void main()
 			iSentBytes = send(client_socket, recv_buffer, strlen(send_buffer), 0);
 			if (iSentBytes == SOCKET_ERROR)
 			{
-				cout << "Send failed with error:\t" << WSAGetLastError() << endl;
+				dwError = WSAGetLastError();
+				cout << FormatLastError(dwError, szError) << endl;
+				//cout << "Send failed with error:\t" << WSAGetLastError() << endl;
 			}
 			else cout << iSentBytes << " Bytes sent" << endl;
 		}
 		else if (iReseivedBytes == 0) cout << "Connection closing..." << endl;
-		else cout << "Receive failed with error: " << WSAGetLastError() << endl;
+		else
+		{
+			dwError = WSAGetLastError();
+			cout << FormatLastError(dwError, szError) << endl;
+		}
+			//cout << "Receive failed with error: " << WSAGetLastError() << endl;
 	} while (iReseivedBytes > 0);
 
 	iResult = shutdown(client_socket, SD_BOTH);
 	if (iResult == SOCKET_ERROR)
 	{
-		cout << "Shutdown failed with error:\t" << WSAGetLastError() << endl;
+		dwError = WSAGetLastError();
+		cout << FormatLastError(dwError, szError) << endl;
+		//cout << "Shutdown failed with error:\t" << WSAGetLastError() << endl;
 	}
 	system("PAUSE");
 	closesocket(listen_socket);
