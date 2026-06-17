ec6124f18b9a9d4b3083cc38234c3f4d29c761e8
AfanasevStas <afanasevstas96@mail.ru>
2026-06-17 16:34:00 +0300

    HW_5 - test c#

--- Test threads-mutex in c#/Program.cs
+++ Test threads-mutex in c#/Program.cs
@@ -2,41 +2,52 @@
 using System;
 class Program
 {
-    static void Plus()
+    static Mutex mutex = new Mutex();
+    static int check = 0;
+    static void Line()
     {
-        int check = 0;
         while (true)
         {
             if (check >= 50)
             {
+                mutex.WaitOne();
                 Console.WriteLine();
+                mutex.ReleaseMutex();
                 check = 0;
             }
+
+        }
+    }
+        static void Plus()
+    {
+        while (true)
+        {
+                mutex.WaitOne();
             Console.Write("+");
-            Thread.Sleep(10);
             check++;
+            Thread.Sleep(10);
+                mutex.ReleaseMutex();
         }
     }
     static void Minus()
     {
-        int check = 0;
         while (true)
         {
-            if (check >= 50)
-            {
-                Console.WriteLine();
-                check = 0;
-            }
+                mutex.WaitOne();
             Console.Write("-");
+            check++;
             Thread.Sleep(10);
+                mutex.ReleaseMutex();
         }
     }
     static void Main()
     {
         Thread t1 = new Thread(Plus);
         Thread t2 = new Thread(Minus);
+        Thread t3 = new Thread(Line);
         t1.Start();
         t2.Start();
+        t3.Start();
     }
 }
 
